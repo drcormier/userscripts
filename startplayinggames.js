4ac124133fd48b71b2089f4b7577bb46392bf2b0
Daniel Cormier <git@drc97.com>
2022-08-20 07:41:04 -0400

    Initial code

---
+++ startplayinggames.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name        New script - startplaying.games
+// @namespace   Violentmonkey Scripts
+// @match       https://startplaying.games/*
+// @grant       none
+// @version     1.0
+// @author      Daniel Cormier
+// @description 8/20/2022, 12:32:45 AM
+// @require http://code.jquery.com/jquery-latest.js
+// ==/UserScript==
+'use strict';
+
+$(document).ready(function() {
+    alert('New script loaded.');
+});
